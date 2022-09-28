a6c8759ea5e57ac5b5cd4d2235d9fedf456245d5
aleksandr <atrostyanko@gmail.com>
2022-09-28 19:32:30 +0200

    done

---
+++ src/test/java/DivTest.java
@@ -0,0 +1,56 @@
+import data.StaticProvider;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.security.DigestException;
+import java.util.List;
+
+public class DivTest extends BaseTest {
+
+    @Test
+    public void testSum() {
+        Assert.assertEquals(calculator.sum(2, 3), 5, "Неверная сумма...");
+    }
+
+    @Test(enabled = false)
+    public void testSum1() {
+        Assert.assertEquals(calculator.sum(2, 3), 5, "Неверная сумма...");
+    }
+
+
+    @Test(description = "Тест с описанием")
+    public void testSum2() {
+        Assert.assertEquals(calculator.sum(2, 3), 5, "Неверная сумма...");
+    }
+
+    @Test(testName = "Test with name")
+    public void testSum3() {
+        Assert.assertEquals(calculator.sum(2, 3), 5, "Неверная сумма...");
+    }
+
+    @Test(timeOut = 1000)
+    public void waitLongTimeTest() throws InterruptedException {
+        Thread.sleep(500);
+    }
+
+    //@Test (invocationCount = 3, invocationTimeOut = 1000, threadPoolSize = 3)
+    @Test (invocationCount = 3, invocationTimeOut = 1000, threadPoolSize = 3)
+    public void invocationCountTest() throws InterruptedException {
+        Thread.sleep(500);
+        Assert.assertEquals(calculator.sum(2, 3), 5, "Неверная сумма...");
+    }
+
+
+    @Test (dataProvider = "dataForSum", dataProviderClass = StaticProvider.class, threadPoolSize = 3)
+    public void testDataProvider(int a, int b, int expectedResult, String text) {
+        System.out.println(text);
+        Assert.assertEquals(calculator.sum(a, b), expectedResult, "Неверная сумма...");
+    }
+
+    @Test (expectedExceptions = {NullPointerException.class, DigestException.class})
+    public void testExceptions() {
+        List list = null;
+        int size = list.size();
+    }
+
+}
